b646e36e4e890ae9457f4754f3817ecbe1a9b37e
yeonjoo.lee <teddyjune@naver.com>
2022-08-16 00:15:33 +0900

    add: 재고 있는 매장수 표시

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('마스크 재고 있는 곳: 0 곳'),
+          title: Text('마스크 재고 있는 곳: ${stores.length} 곳'),
         ),
         body: ListView(
           children: stores.map((e) {
