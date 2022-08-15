e7c83819332d9b052d49502ca242613aa9ab8560
yeonjoo.lee <teddyjune@naver.com>
2022-08-15 22:18:35 +0900

    add: http 사용

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future fetch() async {
+    Uri url = Uri.parse(
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
+    http.Response response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body : ${utf8.decode(response.bodyBytes)}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('마스크 재고 있는 곳: 0 곳'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: fetch,
+          child: Text('테스트'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/store.dart
@@ -0,0 +1,21 @@
+class Store {
+  final double lat;
+  final double lng;
+  final String name;
+  final String remainStat;
+
+  Store(
+      {required this.lat,
+      required this.lng,
+      required this.name,
+      required this.remainStat});
+
+  factory Store.fromJson(Map<String, dynamic> json) {
+    return Store(
+      lat: json['lat'] as double,
+      lng: json['lng'] as double,
+      name: json['name'] as String,
+      remainStat: json['remain_stat'] as String,
+    );
+  }
+}
