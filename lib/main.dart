1988e0ed6faf3aab546a23c82df086394b2a85b1
yeonjoo.lee <teddyjune@naver.com>
2022-08-16 01:25:17 +0900

    add: switch문으로 remainStat 처리

--- lib/main.dart
+++ lib/main.dart
@@ -74,10 +74,51 @@ class _MyHomePageState extends State<MyHomePage> {
                   return ListTile(
                     title: Text(e.name),
                     subtitle: Text(e.address),
-                    trailing: Text(e.remainStat ?? '매진'),
+                    trailing: _buildRemainingStatWidget(e),
                   );
                 }).toList(),
               ));
+  }
+
+  Widget _buildRemainingStatWidget(Store store) {
+    String remainStat = '판매중지';
+    String description = '판매중지';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30~100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2~30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+    }
+
+    return Column(
+      children: [
+        Text(
+          remainStat,
+          style:
+              const TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+        ),
+        Text(description),
+      ],
+    );
   }
 }
 
